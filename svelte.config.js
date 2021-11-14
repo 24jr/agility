08f45966036706e7f1d09a6ac5844f6b58d42c53
Jordan Rose <jordan.rose@theplanworks.com>
2021-11-13 21:05:00 -0500

    upd

--- svelte.config.js
+++ svelte.config.js
@@ -1,20 +1,3 @@
-// /** @type {import('@sveltejs/kit').Config} */
-// const config = {
-// 	kit: {
-// 		// hydrate the <div id="svelte"> element in src/app.html
-// 		target: '#svelte'
-// 	},
-//   vite: {
-//     resolve: {
-//       alias: {
-//         './runtimeConfig': './runtimeConfig.browser',
-//       },
-//     },
-//   },
-// };
-
-// export default config;
-
 import adapter from '@sveltejs/adapter-static';
 
 export default {
@@ -35,29 +18,3 @@ export default {
     },
 	}
 };
-
-
-// import adapter from '@sveltejs/adapter-node'
-
-// /** @type {import('@sveltejs/kit').Config} */
-// export default {
-//   kit: {
-//     target: '#svelte',
-//     adapter: adapter({
-//       // default options are shown
-//       out: 'build',
-//       precompress: false,
-//       env: {
-//         host: 'HOST',
-//         port: 'PORT'
-//       },
-//     }),
-//     vite: {
-//       resolve: {
-//         alias: {
-//           './runtimeConfig': './runtimeConfig.browser',
-//         },
-//       },
-//     },
-//   },
-// }
