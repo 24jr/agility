64d0091a00758e7ba02a264c2e1b537b63768476
Jordan Rose <jordan.rose@theplanworks.com>
2021-11-15 00:16:20 -0500

    update

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,10 @@
+/** @type {import('@sveltejs/kit').Config} */
 import adapter from '@sveltejs/adapter-static';
 
 export default {
 	kit: {
     target: '#svelte',
 		adapter: adapter({
-			// default options are shown
 			pages: 'build',
 			assets: 'build',
 			fallback: null
